3b3d0dde838a1dd473e4ed2d31615fcf319eda69
FahadNassir <nassirfahad69@gmail.com>
2025-05-24 23:34:39 +0300

    Made changes to the index.js export

--- api/index.js
+++ api/index.js
@@ -5,18 +5,13 @@ const cors = require('cors');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Import routes
 const orderRoutes = require('../routes/orderRoutes');
 const history = require('../routes/history');
 
-// Mount routes
 app.use('/', orderRoutes);
 app.use('/history', history);
 
-// Export as serverless function
-module.exports.handler = serverless(app);
-
+module.exports = serverless(app);
