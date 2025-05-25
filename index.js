07b05a4c52d91f4a55e76e295445d9385cbd11f0
FahadNassir <nassirfahad69@gmail.com>
2025-05-25 11:30:56 +0300

    changed from vercel to render

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 require('dotenv').config();
 const express = require('express');
-const serverless = require('serverless-http');
 const cors = require('cors');
-const path = require('path');
+
 
 const app = express();
 
-// Middleware
+//Middleware
 app.use(express.json());
 app.use(cors());
 
-// Routes
+//Test route to confirm deployment is working
+app.get('/ping', (req, res) => {
+  console.log('Ping received');
+  res.send('pong from Vercel');
+});
+
+// Import routes from root-level "routes" folder
 const orderRoutes = require('./routes/orderRoutes');
-const history = require('./routes/history');
+const historyRoutes = require('./routes/history');
 
+//Use routes
 app.use('/', orderRoutes);
-app.use('/history', history);
+app.use('/history', historyRoutes);
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
-}
-
-// For Vercel deployment
-module.exports = serverless(app); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
